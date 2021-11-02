dd283aa9f7e63d689407a33c7288c70625dffc6c
Mark ter Maat <livestockmanagement@nedap.com>
2021-11-02 21:55:53 +0100

    Fixed potential issues

--- src/main.cpp
+++ src/main.cpp
@@ -23,13 +23,15 @@ WiFiClient wifiClient;
 MQTTClient mqttClient;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 
-struct Measurement {
+struct Measurement
+{
   double temperature;
   double humidity;
 };
 
-void initMQTT();
-void initWifi();
+void readAndSendData();
+bool initMQTT();
+bool initWifi();
 void initSensor();
 String getCurrentTime();
 Measurement getMeasurement();
@@ -37,13 +39,29 @@ void sendMeasurement(Measurement m);
 
 void setup()
 {
-  Serial.begin(115200);
-  initWifi();
-  initMQTT();
-  initSensor();
+  readAndSendData();
 
+  Serial.println("Going to sleep");
+  Serial.flush();
+  delay(1000);
+
+  esp_sleep_enable_timer_wakeup(60 * 5 * uS_TO_S_FACTOR);
+  esp_deep_sleep_start();
+}
+
+void loop()
+{
+}
+
+void readAndSendData()
+{
   try
   {
+    Serial.begin(115200);
+    if(!initWifi()) return;
+    if(!initMQTT()) return;
+    initSensor();
+
     Measurement m = getMeasurement();
     sendMeasurement(m);
     Serial.print("Temperature: ");
@@ -55,45 +73,49 @@ void setup()
   {
     Serial.println("Could not get a good measurement from the sensor");
   }
-
-  Serial.println("Going to sleep");
-  Serial.flush();
-  delay(1000);
-
-  esp_sleep_enable_timer_wakeup(10 * uS_TO_S_FACTOR);
-  esp_deep_sleep_start();
 }
 
-void loop()
-{
-}
-
-void initWifi()
+bool initWifi()
 {
   Serial.print("Connecting to Wifi SSID: ");
   Serial.println(ssid);
   Serial.print("Connecting.");
 
   WiFi.begin(ssid, pass);
+  int counter = 0;
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
     delay(500);
+    counter++;
+    if (counter > 100)
+    {
+      return false;
+    }
   }
   Serial.println(" Connected");
+  return true;
 }
 
-void initMQTT()
+bool initMQTT()
 {
   IPAddress ip(192, 168, 2, 4);
-  
+
   Serial.print("Connecting to MQTT: ");
   mqttClient.begin(ip, wifiClient);
-  while(!mqttClient.connect("ESP32-sensor")) {
+  int counter = 0;
+  while (!mqttClient.connect("ESP32-sensor"))
+  {
     Serial.print(".");
-    delay(100); 
+    delay(500);
+    counter++;
+    if (counter > 100)
+    {
+      return false;
+    }
   }
   Serial.println("MQTT Connected");
+  return true;
 }
 
 void initSensor()
@@ -102,7 +124,8 @@ void initSensor()
   dht.temperature().getSensor(&sensor);
 }
 
-Measurement getSingleMeasurement() {
+Measurement getSingleMeasurement()
+{
   Measurement m;
   sensors_event_t event;
 
@@ -126,45 +149,56 @@ Measurement getSingleMeasurement() {
     m.humidity = event.relative_humidity;
   }
 
-  return m; 
+  return m;
 }
 
-String getCurrentTime() {
+String getCurrentTime()
+{
 
   timeClient.begin();
-  timeClient.update();
-  delay(1000);
+
+  int counter = 0;
+  while(!timeClient.forceUpdate()) {
+    delay(500);
+    counter++;
+    if(counter > 10) {
+      throw("Could not retrieve time");
+    }
+  }
   time_t now = timeClient.getEpochTime();
   char buf[sizeof "0000-00-00T00:00:00Z"];
   strftime(buf, sizeof buf, "%FT%TZ", gmtime(&now));
   return buf;
 }
 
-Measurement getMeasurement() {
+Measurement getMeasurement()
+{
   double totalTemperature = 0;
   double totalHumidity = 0;
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 3; i++)
+  {
     Measurement m = getSingleMeasurement();
-    if(isnan(m.temperature) || isnan(m.humidity)) {
-      throw("Could not read sensor"); 
+    if (isnan(m.temperature) || isnan(m.humidity))
+    {
+      throw("Could not read sensor");
     }
     totalTemperature += m.temperature;
     totalHumidity += m.humidity;
     delay(3000);
   }
-  return Measurement {
-    totalTemperature / 3,
-    totalHumidity / 3
-  };
+  return Measurement{
+      totalTemperature / 3,
+      totalHumidity / 3};
 }
 
-String createEvent(String name, double value) {
+String createEvent(String name, double value)
+{
   return "{\"data_type\":\"" + name + "\",\"value\":" + value + "}";
 }
 
-void sendMeasurement(Measurement m) {
+void sendMeasurement(Measurement m)
+{
   String now = getCurrentTime();
   String event = String("{\"data_type\":\"climate\",\"timestamp\":\"" + now + "\",\"temperature\":") + m.temperature + ",\"humidity\":" + m.humidity + "}";
   mqttClient.publish("data/climate/living_room", event);
 }
-
